3db84fbebfce6e4d352ddba921b40877f64a58b2
Hakan Tuncer <hakantuncer@gmail.com>
2012-12-14 00:10:00 +0100

    login page is wired to the controllers

--- EtiketiUstunde.Web/EtiketiUstunde.Web/Scripts/etiketiustunde.login.js
+++ EtiketiUstunde.Web/EtiketiUstunde.Web/Scripts/etiketiustunde.login.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(function () {
+    // Set the events for the external login buttons
+    $('.fb').on('click', function () {
+        $('#provider').val('facebook');
+        $('#ExternalLogin').submit();
+    });
+    
+    $('.tw').on('click', function () {
+        $('#provider').val('twitter');
+        $('#ExternalLogin').submit();
+    });
+});
